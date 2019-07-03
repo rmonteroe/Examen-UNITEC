ffc01450659559059c40fd0fb62b71a23be51484
rmonteroe <ricardomonteroescudero@gmail.com>
2019-07-02 23:53:01 -0500

    v2

--- Agenda Unitec Angular/src/app/app.component.ts
+++ Agenda Unitec Angular/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { GLOBAL } from './services/global';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Contactos Agenda Unitec';
